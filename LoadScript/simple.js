295b9aeb013dfd6f77c7e67cdc318360c8d9fb52
Abhinav Kant <abhinav.kant@gmail.com>
2023-10-05 21:55:10 +0530

    Added grafana

--- LoadScript/simple.js
+++ LoadScript/simple.js
@@ -12,29 +12,29 @@ const PASSWORD = 'password1';
 export const options = {
     stages: [
         { duration: '10s', target: 20 },
-        // { duration: '30s', target: 500 },
-        // { duration: '30s', target: 5000 },
-        // { duration: '5m', target: 10000 },
+        { duration: '30s', target: 500 },
+        { duration: '30s', target: 5000 },
+        { duration: '5m', target: 10000 },
         // { duration: '30s', target: 5000 },
         // { duration: '30s', target: 500 },
         // { duration: '10s', target: 20 },
     ],
 };
 
-export function setup() {
-    return authenticateUser(TENANT_ID, CLIENT_ID, CLIENT_SECRET, "password", USERNAME, PASSWORD);
-}
+// export function setup() {
+//     return authenticateUser(TENANT_ID, CLIENT_ID, CLIENT_SECRET, "password", USERNAME, PASSWORD);
+// }
 
 export default function (data) {
 
-    const params = {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${data.access_token}`, // or `Bearer ${clientAuthResp.access_token}`
-        },
-    };
+    // const params = {
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'Authorization': `Bearer ${data.access_token}`, // or `Bearer ${clientAuthResp.access_token}`
+    //     },
+    // };
 
-    const res = http.get('https://localhost:8081/WeatherForecast', params);
+    const res = http.get('https://localhost:8081/WeatherForecast');
     check(res,
         {
             'status was 200': (r) => r.status == 200,
